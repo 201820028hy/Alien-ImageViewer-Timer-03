b82d932e3835a4f34f685e2867065fa8b58f9650
dit03 <dit@imac-4.local>
2019-03-28 12:33:02 +0900

    Alien ImageViewer Timer

--- Alien ImageViewer Timer 03/ViewController.swift
+++ Alien ImageViewer Timer 03/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var AlienImageView: UIImageView!
+    @IBOutlet weak var CountLabel: UILabel!
+    var count = 1
+    //Timer 객체 생성
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        AlienImageView.image = UIImage(named: "Image\(count)")
+        CountLabel.text = String(count)
     }
 
-
+    @IBAction func btnPlay(_ sender: Any) {
+        //Timer작동
+        myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func btnPause(_ sender: Any) {
+        myTimer.invalidate()
+        
+    }
+    
+    @IBAction func btnStop(_ sender: Any) {
+        myTimer.invalidate()
+        count = 0
+        
+    }
+    
+    @objc func  doAnimation() {
+        if count == 5 {
+            count = 0
+        }
+        count += 1
+        AlienImageView.image = UIImage(named: "Image\(count)")
+        CountLabel.text = String(count)
+    }
+    
 }
 
